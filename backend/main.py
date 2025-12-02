f1cfdff08671a9dad7b150794546652841af0922
Banele Mdluli <banele.mdluli25@gmail.com>
2025-12-02 12:08:27 +0200

    Change the backend main to use router APIs

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, Query
 from joblib import load
+from .routers import prediction,mongodb
 
+#create a Main API instance
 app = FastAPI()
+
+#Link the sub- API to the main API
+app.include_router(prediction.router)
+app.include_router(mongodb.router)
 
 # Usually a post used pydantic
 # class requestPrediction(BaseModel):
@@ -9,9 +15,9 @@ app = FastAPI()
 
 #if running the code locally, comment pipeline = load('model.joblib') and uncomment next line
 #pipeline = load('backend/model.joblib') The change done below is required by docker since it cannot interpret or process parent folders
-pipeline = load('model.joblib')
+# pipeline = load('model.joblib')
 
-@app.get('/makeprediction')
-async def make_prediction(age:int = Query(gt=10,lt=70) ,salary:float = Query(gt=100,lt=100000000)):
-    prediction = pipeline.predict([[age,salary]])
-    return {"prediction": 'will not purchase the product' if int(prediction[0]) == 0 else 'will purchase the product'}
+# @app.get('/makeprediction')
+# async def make_prediction(age:int = Query(gt=10,lt=70) ,salary:float = Query(gt=100,lt=100000000)):
+#     prediction = pipeline.predict([[age,salary]])
+#     return {"prediction": 'will not purchase the product' if int(prediction[0]) == 0 else 'will purchase the product'}
